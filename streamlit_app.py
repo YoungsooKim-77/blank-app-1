8248f83c93e67e5e0fce7af046b31bdef27b3ca8
YoungsooKim-77 <ysphoenix@gmail.com>
2024-11-05 10:48:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,31 @@ st.title("🎈 김영수 app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.text("안녕하세요 김영수입니다.")
+
+
+# Example
+df = pd.DataFrame(
+    np.random.randn(20, 4),
+    columns=["a", "b", "c", "d"])
+
+st.area_chart(
+    df,
+    x="a",
+    y=["b", "c", "d"],
+    color=["#1764AB", "#4A98CA", "#94C5DF"],
+)
+
+
+if st.button('Say Hello'):
+   st.write('Hello, Streamlit!')
+
+
+if st.button('Click Me', key='my_button', help='Click this button to perform an action'):
+   st.write('You clicked the button!')
